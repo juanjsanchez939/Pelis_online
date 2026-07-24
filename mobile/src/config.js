4ad34dbe655e767c03c08f2664837010b3bb2719
juanjsanchez939 <juanjsanchezgarcia939@gmail.com>
2026-07-23 22:45:14 -0300

    Mobile siempre usa API de Render

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,22 +1,2 @@
-import Constants from 'expo-constants';
-
-function getDevServerHost() {
-  const hostUri = Constants.expoConfig?.hostUri;
-  if (hostUri) {
-    return hostUri.split(':')[0];
-  }
-  const debuggerHost = Constants.expoGoConfig?.debuggerHost;
-  if (debuggerHost) {
-    return debuggerHost.split(':')[0];
-  }
-  return null;
-}
-
-const devHost = getDevServerHost();
-
-export const API_BASE_URL = devHost
-  ? `http://${devHost}:3001`
-  : 'https://pelis-online-api.onrender.com';
-
-
+export const API_BASE_URL = 'https://pelis-online-api.onrender.com';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
